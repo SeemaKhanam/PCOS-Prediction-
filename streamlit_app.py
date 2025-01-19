a8df4a1de88932fe910eb668facb9788bb8769f1
SeemaKhanam <148565233+SeemaKhanam@users.noreply.github.com>
2025-01-19 09:56:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ with st.expander('Data'):
   st.write('**Raw Data**')
   df=pd.read_csv("https://raw.githubusercontent.com/SeemaKhanam/dataset/refs/heads/main/Cleaned-Data.csv")
   #Selecting only relevent features 
-  df=df.drop(['Height_ft','Diet_Bread_Cereals','Age','Marital_Status','Exercise_Frequency','Exercise_Type','Exercise_Duration','Smoking','Childhood_Trauma','Cardiovascular_Disease'],axis=1)
+  df=df.drop(['Height_ft','Vegetrian','Diet_Fats','Diet_Sweets','Diet_Fried_Food','Diet_Tea_Coffee','Diet_Multivitamin','Diet_Bread_Cereals','Age','Marital_Status','Exercise_Frequency','Exercise_Type','Exercise_Duration','Smoking','Childhood_Trauma','Cardiovascular_Disease'],axis=1)
   df
   #Splitting x and y 
   X=df.drop(['PCOS'],axis=1)
@@ -20,7 +20,7 @@ with st.expander('Data'):
   X
   st.write("**Y**")
   y
-with st.expander("Visualization"):
-  st.scatter_chart(data=df,y='Hyperandrogenism',x='PCOS')
-  st.scatter_chart(data=df,y='Hirsutism',x='PCOS')
+with st.slider:
+st.header("Input Features")
+Weight_kg	PCOS	Family_History_PCOS	Menstrual_Irregularity	Hormonal_Imbalance	Hyperandrogenism	Hirsutism	Mental_Health	Conception_Difficulty	Insulin_Resistance	...	Diet_Fats	Diet_Sweets	Diet_Fried_Food	Diet_Tea_Coffee	Diet_Multivitamin	Vegetarian	Sleep_Hours	Stress_Level	Exercise_Benefit	PCOS_Medication
   
