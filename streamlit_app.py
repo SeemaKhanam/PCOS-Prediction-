e9fe19ce8546059e5d75c5bd3069b3249d96ba48
SeemaKhanam <148565233+SeemaKhanam@users.noreply.github.com>
2025-01-19 09:34:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('🎈 App Name')
 
-st.write('Hello world!')
+st.title(' 👩‍🦰 PCOS Prediction')
+
+st.info('This app will predict whether you have PCOS')
+
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df=pd.read_csv("https://raw.githubusercontent.com/SeemaKhanam/dataset/refs/heads/main/Cleaned-Data.csv")
+  df
