d4963dce3d6da22f35c2885886658df8e1d23755
SeemaKhanam <148565233+SeemaKhanam@users.noreply.github.com>
2025-01-19 19:57:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ t = np.hstack([weight_array_train, x_train_new])
 X_train, X_test, y_train, y_test = train_test_split(t, y_new, test_size=0.2, random_state=0)
 
 # Train the Logistic Regression model
-LR = LogisticRegression()
+LR = LogisticRegression(max_iter=1000)
 LR.fit(X_train, y_train)
 
 # Prepare the input for prediction
