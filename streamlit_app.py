20aee9243bb277e2e4512c6ce031617b76c41012
SeemaKhanam <148565233+SeemaKhanam@users.noreply.github.com>
2025-01-19 09:36:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,6 @@ st.info('This app will predict whether you have PCOS')
 with st.expander('Data'):
   st.write('**Raw Data**')
   df=pd.read_csv("https://raw.githubusercontent.com/SeemaKhanam/dataset/refs/heads/main/Cleaned-Data.csv")
+  #Selecting only relevent features 
+  df=df.drop(['Height_ft','Diet_Bread_Cereals','Age','Marital_Status','Exercise_Frequency','Exercise_Type','Exercise_Duration','Smoking','Childhood_Trauma','Cardiovascular_Disease'],axis=1)
   df
